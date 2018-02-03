3b484eed9e541874c74b5a56902a84412576c5dd
Janet <janet.song@gmail.com>
2018-02-03 16:27:41 -0600

    Checkpoint 22 DOM Scripting:Animation

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,36 +1,16 @@
+
 var animatePoints = function() {
+  var points = document.getElementsByClassName('point');
 
-          var points = document.getElementsByClassName('point');
-
-          var revealFirstPoint = function() {
-              points[0].style.opacity = 1;
-              points[0].style.transform = "scaleX(1) translateY(0)";
-              points[0].style.msTransform = "scaleX(1) translateY(0)";
-              points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-              points[0].style.WebkitTransition = "all 0.5s";
-              points[0].style.transition = "all 0.5s";
-          };
-
-          var revealSecondPoint = function() {
-              points[1].style.opacity = 1;
-              points[1].style.transform = "scaleX(1) translateY(0)";
-              points[1].style.msTransform = "scaleX(1) translateY(0)";
-              points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-              points[1].style.WebkitTransition = "all 1s";
-              points[1].style.transition = "all 1s";
-          };
-
-          var revealThirdPoint = function() {
-              points[2].style.opacity = 1;
-              points[2].style.transform = "scaleX(1) translateY(0)";
-              points[2].style.msTransform = "scaleX(1) translateY(0)";
-              points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-              points[2].style.WebkitTransition = "all 1.5s";
-              points[2].style.transition = "all 1.5s";
-          };
-
-          revealFirstPoint();
-          revealSecondPoint();
-          revealThirdPoint();
-
-      };
+  var revealPoint = function(index) {
+              points[index].style.opacity = 1;
+              points[index].style.transform = "scaleX(1) translateY(0)";
+              points[index].style.msTransform = "scaleX(1) translateY(0)";
+              points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+              points[index].style.WebkitTransition = "all 0.2s";
+              points[index].style.transition = "all 0.5s";
+  }
+for (var i=0; i < points.length; i++) {
+revealPoint(i);
+  }
+};
