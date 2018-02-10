01e58b76f38746785ee142a29e63bd0f26378452
Janet <janet.song@gmail.com>
2018-02-10 15:35:54 -0600

    Checkpoint 26 DOM:Play1 completed

--- scripts/album.js
+++ scripts/album.js
@@ -28,7 +28,7 @@ var albumPicasso = {
      ]
  };
 
- var albumMarconi = {
+ var albumYouth1982 = {
      title: 'Zoe and Superman',
      artist: 'Youth 1982',
      label: 'Mecca',
@@ -46,7 +46,7 @@ var albumPicasso = {
  var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
-      + '  <td class="song-item-number">' + songNumber + '</td>'
+      + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
       + '  <td class="song-item-title">' + songName + '</td>'
       + '  <td class="song-item-duration">' + songLength + '</td>'
       + '</tr>'
@@ -54,13 +54,13 @@ var albumPicasso = {
 
      return template;
  };
+ var albumTitle = document.getElementsByClassName('album-view-title')[0];
+ var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+ var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+ var albumImage = document.getElementsByClassName('album-cover-art')[0];
+ var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
  var setCurrentAlbum = function(album) {
-     var albumTitle = document.getElementsByClassName('album-view-title')[0];
-     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-     var albumImage = document.getElementsByClassName('album-cover-art')[0];
-     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
      albumTitle.firstChild.nodeValue = album.title;
      albumArtist.firstChild.nodeValue = album.artist;
@@ -74,6 +74,35 @@ var albumPicasso = {
      }
  };
 
+ var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+ var songRows = document.getElementsByClassName('album-view-song-item');
+
+ var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+
+
  window.onload = function() {
-     setCurrentAlbum(Marconi);
- };
+     setCurrentAlbum(albumPicasso);
+
+     songListContainer.addEventListener('mouseover', function(event) {
+    // #1
+    if (event.target.parentElement.className === 'album-view-song-item') {
+ event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+       }
+});
+
+for (var i = 0; i < songRows.length; i++) {
+         songRows[i].addEventListener('mouseleave', function(event) {
+           this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+      });
+  }
+         };
+
+     var albums= [albumPicasso, albumMarconi, albumYouth1982];
+     var index = 1;
+     albumImage.addEventListener("click", function(event) {
+       setCurrentAlbum(albums[index]);
+       index++;
+       if (index == albums.length) {
+         index = 0;
+       }
+     });
